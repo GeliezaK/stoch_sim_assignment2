0872b958bbcd18eabd4f598ec4d705795d3cf2b8
GeliezaK <gelieza98@t-online.de>
2022-12-01 12:38:30 +0100

    Extract experiment method

--- DES.py
+++ DES.py
@@ -1,42 +1,54 @@
 import random
+
+import pandas as pd
 import simpy
 import numpy as np
 
-lambd = 1 #arrival rate
-mu = 1.2 #server capacity/rate
 
-n_servers = 2
-n_costumers = 100
-
-wait_times = []
-
-def source(env, n_costumers, lambd, mu, server, wait_times):
+def arrivals(env, n_costumers, lambd, mu, server, wait_times):
     for i in range(n_costumers):
-        p = process(env, 'Process_%02d' % i, server, mu, wait_times)
-        env.process(p)
-        wait = random.expovariate(lambd)
-        yield env.timeout(wait)
+        e = event(env, 'Event_%02d' % i, server, mu, wait_times)
+        env.process(e)
+        inter_arrival_time = random.expovariate(lambd)
+        yield env.timeout(inter_arrival_time)
 
 
-def process(env, name, server, mu, wait_times):
+def event(env, name, server, mu, wait_times):
     arrive = env.now
-    print('%7.4f %s: Here I am' % (arrive, name))
 
     with server.request() as req:
         yield req
         
-        process_duration = random.expovariate(mu)
+        service_duration = random.expovariate(mu)
         end_queue = env.now
         wait_times.append(end_queue - arrive)
 
-        print('%s starting at %7.4f' % (name, end_queue))
-        yield env.timeout(process_duration)
-        print('%s finished at %7.4f' % (name, env.now))
+        yield env.timeout(service_duration)
+
+def experiment(l, mu, n_servers, n_costumers):
+    print("Rho = ", np.round(l / mu*n_servers, 3))
+    wait_times = []
+    env = simpy.Environment()
+    server = simpy.Resource(env, capacity=n_servers)
+    env.process(arrivals(env, n_costumers, l, mu, server, wait_times))
+    env.run()
+    return wait_times
+
+if __name__ == '__main__':
+    lambd = 1  # arrival rate
+    mu = 1.1  # server capacity/rate
+
+    n_servers = [1, 2, 4]
+    n_costumers = 100
+
+    df = pd.DataFrame()
+
+    for ind, c in enumerate(n_servers):
+        lambd_c = lambd/c
+        wait_times = experiment(lambd_c, mu, c, n_costumers)
+        df[f"{c}"] = wait_times
+
+    print(df)
+    print(df.mean())
 
 
-env = simpy.Environment()
-server = simpy.Resource(env, capacity=n_servers)
-env.process(source(env, n_costumers, lambd, mu, server, wait_times))
-env.run()
-
-print(f"\nAverage wait time of {np.mean(wait_times)}")
