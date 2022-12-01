Fix computation of rho

DES.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def arrivals(env, n_costumers, lambd, mu, server, wait_times):
     for i in range(n_costumers):
-        e = event(env, 'Event_%02d' % i, server, mu, wait_times)
+        e = event(env, server, mu, wait_times)
         env.process(e)
         inter_arrival_time = random.expovariate(lambd)
         yield env.timeout(inter_arrival_time)
@@ -24,28 +24,28 @@ def event(env, server, mu, wait_times):
 
         yield env.timeout(service_duration)
 
-def experiment(l, mu, n_servers, n_costumers):
-    print("Rho = ", np.round(l / mu*n_servers, 3))
+def experiment(l, mu, n_servers, n_customers):
+    print("Rho = ", np.round(l / (mu*n_servers), 3))
     wait_times = []
     env = simpy.Environment()
     server = simpy.Resource(env, capacity=n_servers)
-    env.process(arrivals(env, n_costumers, l, mu, server, wait_times))
+    env.process(arrivals(env, n_customers, l, mu, server, wait_times))
     env.run()
     return wait_times
 
 
 if __name__ == '__main__':
-    lambd = 1  # arrival rate
-    mu = 1.1  # server capacity/rate
+    lambd = 0.9  # arrival rate
+    mu = 1  # server capacity (rate, e.g. 1.2 customers/unit time)
 
     n_servers = [1, 2, 4]
-    n_costumers = 100
+    n_customers = 1000
 
     df = pd.DataFrame()
 
     for ind, c in enumerate(n_servers):
-        lambd_c = lambd/c
-        wait_times = experiment(lambd_c, mu, c, n_costumers)
+        lambd_c = lambd*c
+        wait_times = experiment(lambd_c, mu, c, n_customers)
         df[f"{c}"] = wait_times
 
     print(df)